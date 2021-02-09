341c073cf31bf3232f09c022a9832ef9f670466b
@yopigambyok <yopigambyok@gmail.com>
2021-02-09 09:41:16 +0700

    halaman product desktop aman

--- Web Arahan/1.0.0/assets/js/style.js
+++ Web Arahan/1.0.0/assets/js/style.js
@@ -30,3 +30,71 @@ $(window).scroll(function(){
 $('.nav-mobile .content .btn-menu i').click(function(){
     $('.menu#mobile').toggleClass('active');
 });
+
+
+
+
+$('.product-page .cari-tryout .content .content-heading .navigation ul a.all').click(function(){
+    $('.product-page .card-tryout .content .card.cardUtbk').removeClass('unActive')
+    $('.product-page .card-tryout .content .card.cardBebas').removeClass('unActive')
+    $('.product-page .card-tryout .content .card.cardPaket').removeClass('unActive')
+    $('.product-page .card-tryout .content .card.cardKuno').removeClass('unActive')
+
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.all').addClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aUtbk').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aBebas').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aPaket').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').removeClass('active')
+});
+
+$('.product-page .cari-tryout .content .content-heading .navigation ul a.aUtbk').click(function(){
+    $('.product-page .card-tryout .content .card.cardUtbk').removeClass('unActive')
+    $('.product-page .card-tryout .content .card.cardBebas').addClass('unActive')
+    $('.product-page .card-tryout .content .card.cardPaket').addClass('unActive')
+    $('.product-page .card-tryout .content .card.cardKuno').addClass('unActive')
+
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.all').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aUtbk').addClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aBebas').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aPaket').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').removeClass('active')
+});
+
+$('.product-page .cari-tryout .content .content-heading .navigation ul a.aBebas').click(function(){
+    $('.product-page .card-tryout .content .card.cardUtbk').addClass('unActive')
+    $('.product-page .card-tryout .content .card.cardBebas').removeClass('unActive')
+    $('.product-page .card-tryout .content .card.cardPaket').addClass('unActive')
+    $('.product-page .card-tryout .content .card.cardKuno').addClass('unActive')
+
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.all').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aUtbk').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aBebas').addClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aPaket').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').removeClass('active')
+});
+
+$('.product-page .cari-tryout .content .content-heading .navigation ul a.aPaket').click(function(){
+    $('.product-page .card-tryout .content .card.cardUtbk').addClass('unActive')
+    $('.product-page .card-tryout .content .card.cardBebas').addClass('unActive')
+    $('.product-page .card-tryout .content .card.cardPaket').removeClass('unActive')
+    $('.product-page .card-tryout .content .card.cardKuno').addClass('unActive')
+
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.all').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aUtbk').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aBebas').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aPaket').addClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').removeClass('active')
+});
+
+$('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').click(function(){
+    $('.product-page .card-tryout .content .card.cardUtbk').addClass('unActive')
+    $('.product-page .card-tryout .content .card.cardBebas').addClass('unActive')
+    $('.product-page .card-tryout .content .card.cardPaket').addClass('unActive')
+    $('.product-page .card-tryout .content .card.cardKuno').removeClass('unActive')
+
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.all').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aUtbk').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aBebas').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aPaket').removeClass('active')
+    $('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').addClass('active')
+});
