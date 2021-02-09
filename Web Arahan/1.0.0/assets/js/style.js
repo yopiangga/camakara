fef6825686844a77d3355a640a2ff94bb6276b32
@yopigambyok <yopigambyok@gmail.com>
2021-02-09 14:15:28 +0700

    siap presentasi

--- Web Arahan/1.0.0/assets/js/style.js
+++ Web Arahan/1.0.0/assets/js/style.js
@@ -1,12 +1,12 @@
 
-$('.beli-tryout-detail .info-mapel-tryout .content nav .nav-left').click(function(){
+$('.beli-tryout-detail .info-mapel-tryout .content nav .nav-left').click(function () {
     $('.beli-tryout-detail .info-mapel-tryout .content nav .nav-left').addClass('active');
     $('.beli-tryout-detail .info-mapel-tryout .content nav .nav-right').removeClass('active');
     $('.beli-tryout-detail .info-mapel-tryout .content .content-body .card-tka').addClass('disable');
     $('.beli-tryout-detail .info-mapel-tryout .content .content-body .card-tps').removeClass('disable');
 });
 
-$('.beli-tryout-detail .info-mapel-tryout .content nav .nav-right').click(function(){
+$('.beli-tryout-detail .info-mapel-tryout .content nav .nav-right').click(function () {
     $('.beli-tryout-detail .info-mapel-tryout .content nav .nav-left').removeClass('active');
     $('.beli-tryout-detail .info-mapel-tryout .content nav .nav-right').addClass('active');
     $('.beli-tryout-detail .info-mapel-tryout .content .content-body .card-tka').removeClass('disable');
@@ -14,27 +14,27 @@ $('.beli-tryout-detail .info-mapel-tryout .content nav .nav-right').click(functi
 });
 
 
-$(window).scroll(function(){
+$(window).scroll(function () {
     var scroll = $(window).scrollTop();
 
-    if(scroll > 50){
+    if (scroll > 50) {
         $(".nav-desktop").addClass("active");
         $(".nav-mobile").addClass("active");
     } else {
         $(".nav-desktop").removeClass("active");
         $(".nav-mobile").removeClass("active");
     }
-    
+
 })
 
-$('.nav-mobile .content .btn-menu i').click(function(){
+$('.nav-mobile .content .btn-menu i').click(function () {
     $('.menu#mobile').toggleClass('active');
 });
 
 
 
 
-$('.product-page .cari-tryout .content .content-heading .navigation ul a.all').click(function(){
+$('.product-page .cari-tryout .content .content-heading .navigation ul a.all').click(function () {
     $('.product-page .card-tryout .content .card.cardUtbk').removeClass('unActive')
     $('.product-page .card-tryout .content .card.cardBebas').removeClass('unActive')
     $('.product-page .card-tryout .content .card.cardPaket').removeClass('unActive')
@@ -47,7 +47,7 @@ $('.product-page .cari-tryout .content .content-heading .navigation ul a.all').c
     $('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').removeClass('active')
 });
 
-$('.product-page .cari-tryout .content .content-heading .navigation ul a.aUtbk').click(function(){
+$('.product-page .cari-tryout .content .content-heading .navigation ul a.aUtbk').click(function () {
     $('.product-page .card-tryout .content .card.cardUtbk').removeClass('unActive')
     $('.product-page .card-tryout .content .card.cardBebas').addClass('unActive')
     $('.product-page .card-tryout .content .card.cardPaket').addClass('unActive')
@@ -60,7 +60,7 @@ $('.product-page .cari-tryout .content .content-heading .navigation ul a.aUtbk')
     $('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').removeClass('active')
 });
 
-$('.product-page .cari-tryout .content .content-heading .navigation ul a.aBebas').click(function(){
+$('.product-page .cari-tryout .content .content-heading .navigation ul a.aBebas').click(function () {
     $('.product-page .card-tryout .content .card.cardUtbk').addClass('unActive')
     $('.product-page .card-tryout .content .card.cardBebas').removeClass('unActive')
     $('.product-page .card-tryout .content .card.cardPaket').addClass('unActive')
@@ -73,7 +73,7 @@ $('.product-page .cari-tryout .content .content-heading .navigation ul a.aBebas'
     $('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').removeClass('active')
 });
 
-$('.product-page .cari-tryout .content .content-heading .navigation ul a.aPaket').click(function(){
+$('.product-page .cari-tryout .content .content-heading .navigation ul a.aPaket').click(function () {
     $('.product-page .card-tryout .content .card.cardUtbk').addClass('unActive')
     $('.product-page .card-tryout .content .card.cardBebas').addClass('unActive')
     $('.product-page .card-tryout .content .card.cardPaket').removeClass('unActive')
@@ -86,7 +86,7 @@ $('.product-page .cari-tryout .content .content-heading .navigation ul a.aPaket'
     $('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').removeClass('active')
 });
 
-$('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').click(function(){
+$('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').click(function () {
     $('.product-page .card-tryout .content .card.cardUtbk').addClass('unActive')
     $('.product-page .card-tryout .content .card.cardBebas').addClass('unActive')
     $('.product-page .card-tryout .content .card.cardPaket').addClass('unActive')
@@ -98,3 +98,57 @@ $('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno')
     $('.product-page .cari-tryout .content .content-heading .navigation ul a.aPaket').removeClass('active')
     $('.product-page .cari-tryout .content .content-heading .navigation ul a.aKuno').addClass('active')
 });
+
+
+var cardTestimoniDesktop = document.querySelectorAll('.testimonial-page .testimonial-section#desktop .content .content-body .card-group .card-slide .card');
+var navigasiTestimonialDesktop = 0;
+$('.testimonial-page .testimonial-section#desktop .content .content-body .card-group .card-slide').css({"left" : 0, "transition" : "900ms"});
+
+$('.testimonial-page .testimonial-section#desktop .content .content-body .circle-right').click(function () {
+    if( navigasiTestimonialDesktop <= (cardTestimoniDesktop.length - 2) * 340 ){
+        navigasiTestimonialDesktop = navigasiTestimonialDesktop + 340;
+        $('.testimonial-page .testimonial-section#desktop .content .content-body .card-group .card-slide').css({"left" : -navigasiTestimonialDesktop, "transition" : "900ms"});
+    }
+});
+
+$('.testimonial-page .testimonial-section#desktop .content .content-body .circle-left').click(function () {
+    if(navigasiTestimonialDesktop >= 340 && navigasiTestimonialDesktop <= cardTestimoniDesktop.length * 430 ){
+        navigasiTestimonialDesktop = navigasiTestimonialDesktop - 340;
+        $('.testimonial-page .testimonial-section#desktop .content .content-body .card-group .card-slide').css({"left" : -navigasiTestimonialDesktop, "transition" : "900ms"});
+    }
+});
+
+
+var cardTestimoniMobile = document.querySelectorAll('.testimonial-page .testimonial-section#mobile .content .content-body .card-group .card-slide .card');
+var navigasiTestimonialMobile = 0;
+$('.testimonial-page .testimonial-section#mobile .content .content-body .card-group .card-slide').css({"left" : 0, "transition" : "900ms"});
+
+$('.testimonial-page .testimonial-section#mobile .content .content-body .circle-right').click(function () {
+    if( navigasiTestimonialMobile <= (cardTestimoniMobile.length - 2) * 310 ){
+        navigasiTestimonialMobile = navigasiTestimonialMobile + 310;
+        $('.testimonial-page .testimonial-section#mobile .content .content-body .card-group .card-slide').css({"left" : -navigasiTestimonialMobile, "transition" : "900ms"});
+    }
+});
+
+$('.testimonial-page .testimonial-section#mobile .content .content-body .circle-left').click(function () {
+    if(navigasiTestimonialMobile >= 310 && navigasiTestimonialMobile <= cardTestimoniMobile.length * 340 ){
+        navigasiTestimonialMobile = navigasiTestimonialMobile - 310;
+        $('.testimonial-page .testimonial-section#mobile .content .content-body .card-group .card-slide').css({"left" : -navigasiTestimonialDesktop, "transition" : "900ms"});
+    }
+});
+
+
+// $('.testimonial-page .testimonial-section .content .content-body .circle-right').click(function () {
+//     if( navigasiTestimonial <= (cardTestimoni.length - 3) * 340 ){
+//         navigasiTestimonial = navigasiTestimonial + 340;
+//         $('.testimonial-page .testimonial-section .content .content-body .card-group .card-slide').css({"left" : -navigasiTestimonial, "transition" : "900ms"});
+//     }
+// });
+
+// $('.testimonial-page .testimonial-section .content .content-body .circle-left').click(function () {
+//     if(navigasiTestimonial >= 340 && navigasiTestimonial <= cardTestimoni.length * 430 ){
+//         navigasiTestimonial = navigasiTestimonial - 340;
+//         $('.testimonial-page .testimonial-section .content .content-body .card-group .card-slide').css({"left" : -navigasiTestimonial, "transition" : "900ms"});
+//     }
+// });
+
