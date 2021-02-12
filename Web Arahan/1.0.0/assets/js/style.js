3e01b179cb619cb17293fb578a12fdcf21a5cf9c
@yopigambyok <yopigambyok@gmail.com>
2021-02-12 11:10:44 +0700

    page top up dan transfer sesama aman untuk desktop

--- Web Arahan/1.0.0/assets/js/style.js
+++ Web Arahan/1.0.0/assets/js/style.js
@@ -167,3 +167,200 @@ $('.tryout-detail .info-mapel-tryout .content nav .nav-right').click(function ()
     $('.tryout-detail .info-mapel-tryout .content .content-body .card-tka').removeClass('disable');
     $('.tryout-detail .info-mapel-tryout .content .content-body .card-tps').addClass('disable');
 });
+
+
+$('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select').click(function(){
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select .icon-right i').toggleClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body').toggleClass('active');
+})
+
+$('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bca').click(function(){
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bca .right i').addClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-mandiri .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bni .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bri .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-alfamart .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-ovo .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-shopee .right i').removeClass('active');
+
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .nama-bank').html("Bank BCA")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .no-rekening').html("112085775526608")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .atas-nama').html("demitesfaye")
+})
+
+$('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-mandiri').click(function(){
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bca .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-mandiri .right i').addClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bni .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bri .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-alfamart .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-ovo .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-shopee .right i').removeClass('active');
+
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .nama-bank').html("Bank Mandiri")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .no-rekening').html("112085775526608")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .atas-nama').html("demitesfaye")
+})
+
+$('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bni').click(function(){
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bca .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-mandiri .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bni .right i').addClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bri .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-alfamart .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-ovo .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-shopee .right i').removeClass('active');
+
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .nama-bank').html("Bank BNI")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .no-rekening').html("112085775526608")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .atas-nama').html("demitesfaye")
+})
+
+$('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bri').click(function(){
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bca .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-mandiri .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bni .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bri .right i').addClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-alfamart .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-ovo .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-shopee .right i').removeClass('active');
+
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .nama-bank').html("Bank BRI")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .no-rekening').html("112085775526608")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .atas-nama').html("demitesfaye")
+})
+
+$('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-alfamart').click(function(){
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bca .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-mandiri .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bni .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bri .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-alfamart .right i').addClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-ovo .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-shopee .right i').removeClass('active');
+
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .nama-bank').html("Alfamart / Alfamidi")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .no-rekening').html("112085775526608")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .atas-nama').html("demitesfaye")
+})
+
+$('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-ovo').click(function(){
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bca .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-mandiri .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bni .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bri .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-alfamart .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-ovo .right i').addClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-shopee .right i').removeClass('active');
+
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .nama-bank').html("OVO")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .no-rekening').html("112085775526608")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .atas-nama').html("demitesfaye")
+})
+
+$('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-shopee').click(function(){
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bca .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-mandiri .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bni .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group-select .group-right .select-body .btn-bri .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-alfamart .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-ovo .right i').removeClass('active');
+    $('.topup-page .main .content .content-body .card-metode .form .form-group .group-right .btn-shopee .right i').addClass('active');
+
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .nama-bank').html("Shopee Pay")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .no-rekening').html("112085775526608")
+    $('.topup-page .main .content .content-body .card-transfer-detail .form .form-group .atas-nama').html("demitesfaye")
+})
+
+
+
+
+// Card Jumlah Top Up
+
+$('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-10').click(function(){
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-10').addClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-20').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-50').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-100').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-200').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-300').removeClass('active');
+
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group input').val(10000);
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-topup h4 span').html(10000)
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-pembayaran h4 span text').html(10000)
+})
+
+$('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-20').click(function(){
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-10').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-20').addClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-50').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-100').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-200').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-300').removeClass('active');
+
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group input').val(20000);
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-topup h4 span').html(20000)
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-pembayaran h4 span text').html(20000)
+})
+
+$('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-50').click(function(){
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-10').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-20').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-50').addClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-100').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-200').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-300').removeClass('active');
+
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group input').val(50000);
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-topup h4 span').html(50000)
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-pembayaran h4 span text').html(50000)
+})
+
+$('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-100').click(function(){
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-10').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-20').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-50').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-100').addClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-200').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-300').removeClass('active');
+
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group input').val(100000);
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-topup h4 span').html(100000)
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-pembayaran h4 span text').html(100000)
+})
+
+$('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-200').click(function(){
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-10').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-20').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-50').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-100').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-200').addClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-300').removeClass('active');
+
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group input').val(200000);
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-topup h4 span').html(200000)
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-pembayaran h4 span text').html(200000)
+})
+
+$('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-300').click(function(){
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-10').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-20').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-50').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-100').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-200').removeClass('active');
+    $('.topup-page .main .content .content-body .card-jumlah .form .choice-group #choice-300').addClass('active');
+
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group input').val(300000);
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-topup h4 span').html(300000)
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-pembayaran h4 span text').html(300000)
+})
+
+function inputTopUp(){
+    let value = document.querySelector('.topup-page .main .content .content-body .card-jumlah .form .form-group input').value
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-topup h4 span').html(value)
+    $('.topup-page .main .content .content-body .card-jumlah .form .form-group .jumlah-pembayaran h4 span text').html(value)
+}
+
+function checkUserTransfer() {
+    $('.transferSesama-page .main .content .content-body .card-transfer-detail').addClass('active')
+}
